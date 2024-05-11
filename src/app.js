6e6de32029f90ee1c5cf993fe060472d87d91aad
SKILL-DANIEL <ospinad293@gmail.com>
2024-05-10 23:42:22 -0500

    Cambios adicionales

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 // middleware
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost');
+    res.header('Access-Control-Allow-Origin', 'https://devdaniel.web.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
